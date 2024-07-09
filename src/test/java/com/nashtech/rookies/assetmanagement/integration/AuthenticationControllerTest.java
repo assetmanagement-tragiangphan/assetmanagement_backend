52ff973fd386313fe8da2c903f8c22c19bbbe265
Nguyên Phạm <cd.synguyen@gmail.com>
2024-07-09 09:32:02 +0000

    Merged PR 792: [Intergration Test] Authentication Controller
    add test
    
    Related work items: #3690

--- src/test/java/com/nashtech/rookies/assetmanagement/integration/AuthenticationControllerTest.java
+++ src/test/java/com/nashtech/rookies/assetmanagement/integration/AuthenticationControllerTest.java
@@ -6,6 +6,7 @@ import com.nashtech.rookies.assetmanagement.dto.request.AuthenticationRequest;
 import com.nashtech.rookies.assetmanagement.dto.response.LoginResponse;
 import com.nashtech.rookies.assetmanagement.dto.response.ResponseDto;
 import com.nashtech.rookies.assetmanagement.service.AuthenticationService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -32,12 +34,19 @@ class AuthenticationControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private AuthenticationRequest request;
+    private AuthenticationRequest validRequest;
+
+    private AuthenticationRequest invalidRequest;
 
     @BeforeEach
     void setUp() {
-        request = AuthenticationRequest.builder()
+        validRequest = AuthenticationRequest.builder()
                 .username("nguyenp")
+                .password("Nguyen@123")
+                .build();
+
+        invalidRequest = AuthenticationRequest.builder()
+                .username("nguyen")
                 .password("Nguyen@123")
                 .build();
     }
@@ -48,10 +57,21 @@ class AuthenticationControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/v1/auth/signIn")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
+                        .content(objectMapper.writeValueAsString(validRequest))
                         .accept(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testSignIn_whenInvalidRequest_thenThrow() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/signIn")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().is4xxClientError())
+                        .andExpect(jsonPath("$.message", Matchers.is("Username or password is incorrect. Please try again.")));
     }
 
 }
